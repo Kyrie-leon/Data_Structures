f4274287da55b1463db7cdd3ff8057896ee4892c
kyrie-leon <gaomeng9518@163.com>
2021-01-06 22:23:39 +0800

    栈测试函数

---
+++ stack_queue/test.c
@@ -0,0 +1,24 @@
+#include "stack.h"
+
+void TestStack()
+{
+	Stack ps;
+	StackInit(&ps);
+	StackPush(&ps, 1);
+	StackPush(&ps, 2);
+	StackPush(&ps, 3);
+	StackPush(&ps, 4);
+	StackPush(&ps, 5);
+	while (!StackEmpty(&ps))
+	{
+		printf("%d\n", StackTop(&ps));
+		StackPop(&ps);
+	}
+}
+
+int main()
+{
+	TestStack();
+	system("pause");
+	return 0;
+}
