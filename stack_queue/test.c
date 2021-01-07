99c9ebd93582edaf0f5f339d29bc281da4bc68fe
kyrie-leon <gaomeng9518@163.com>
2021-01-07 18:18:38 +0800

    栈和队列的接口测试

--- stack_queue/test.c
+++ stack_queue/test.c
@@ -1,4 +1,5 @@
 #include "stack.h"
+#include "Queue.h"
 
 void TestStack()
 {
@@ -19,9 +20,27 @@ void TestStack()
 	
 }
 
+void TestQueue()
+{
+	Queue q;
+	QueueInit(&q);
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
+	QueuePush(&q, 5);
+	while (!(QueueEmpty(&q)))
+	{
+		printf("%d ", QueueFront(&q));
+		QueuePop(&q);
+	}
+	QueueDestroy(&q);
+}
+
 int main()
 {
-	TestStack();
+	//TestStack();
+	TestQueue();
 	system("pause");
 	return 0;
 }
