759b6ef045b5fd3b65e15c3f4f8e9fed6edec1a4
kyrie-leon <gaomeng9518@163.com>
2021-01-10 21:46:10 +0800

    增加Top问题

--- Heap/Heap.h
+++ Heap/Heap.h
@@ -4,7 +4,6 @@
 #include<string.h>
 #include<assert.h>
 
-//大堆实现
 typedef int HPDataType;
 
 typedef struct Heap
@@ -40,3 +39,12 @@ void HeapSort(HPDataType *a, int n);
 
 //堆打印
 void HeapPrint(HPDataType *php, int n);
+
+
+// TopK问题：找出N个数里面最大/最小的前K个问题。
+// 比如：未央区排名前10的泡馍，西安交通大学王者荣耀排名前10的韩信，全国排名前10的李白。等等问题都是Topk问题，
+// 需要注意：
+// 找最大的前K个，建立K个数的小堆
+// 找最小的前K个，建立K个数的大堆
+void PrintTopK(int* a, int n, int k);
+
