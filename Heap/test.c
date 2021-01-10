0bc93baa1590298814748de7635bfaf544328e10
kyrie-leon <gaomeng9518@163.com>
2021-01-10 20:46:21 +0800

    堆接口测试

--- Heap/test.c
+++ Heap/test.c
@@ -1,7 +1,53 @@
 #include"Heap.h"
 
+void TestHeap()
+{
+	//int a[] = { 27,15,19,18,28,34,65,49,25,37 };
+	//
+	//HeapSort(a, sizeof(a) / sizeof(HPDataType));
+	//HeapPrint(a,sizeof(a)/sizeof(HPDataType));
+
+	int a[] = { 27,15,19,18,28,34,65,49,25,37 };
+	Heap hp;
+	HeapInit(&hp, a, sizeof(a) / sizeof(HPDataType));
+	HeapPrint(hp._a, hp._size);
+	HeapPush(&hp, 10);
+	HeapPush(&hp, 11);
+	HeapPush(&hp, 42);
+	HeapPrint(hp._a, hp._size);
+	HeapPop(&hp);
+	//HeapPop(&hp);
+	//HeapPop(&hp);
+	//HeapPop(&hp);
+	//HeapPop(&hp);
+	//HeapPop(&hp);
+	//HeapPop(&hp);
+	//HeapPop(&hp);
+	//HeapPop(&hp);
+	//HeapPop(&hp);
+	//HeapPop(&hp);
+	//HeapPop(&hp);
+	//HeapPop(&hp);
+	HeapPrint(hp._a, hp._size);
+	printf("%d\n",HeapTop(&hp));
+	printf("%d\n", HeapSize(&hp));
+	printf("%d\n", HeapEmpty(&hp));
+	HeapDestory(&hp);
+	//printf("%d\n", HeapTop(&hp));
+	printf("%d\n", HeapSize(&hp));
+	printf("%d\n", HeapEmpty(&hp));
+
+
+
+
+
+
+}
+
 int main()
 {
-	int a[] = { 27,15,19,18,28,34,65,49,25,37 };
+	TestHeap();
 
+	system("pause");
+	return 0;
 }
