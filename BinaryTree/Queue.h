0352918a627d0ca7111e8e9b67ffb7aff1bd771e
kyrie-leon <gaomeng9518@163.com>
2021-01-18 21:40:24 +0800

    层序遍历——队列

---
+++ BinaryTree/Queue.h
@@ -0,0 +1,38 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<windows.h>
+
+typedef int QDataType;
+
+//队列的链表
+typedef struct QListNode
+{
+	struct QListNode* _next;
+	QDataType _data;
+}QNode;
+
+//队列的结构
+typedef struct Queue
+{
+	QNode *_front;	//队头
+	QNode *_rear;	//队尾
+}Queue;
+
+// 初始化队列 
+void QueueInit(Queue* pq);
+// 队尾入队列 
+void QueuePush(Queue* pq, QDataType data);
+// 队头出队列 
+void QueuePop(Queue* pq);
+// 获取队列头部元素 
+QDataType QueueFront(Queue* pq);
+// 获取队列队尾元素 
+QDataType QueueBack(Queue* pq);
+// 获取队列中有效元素个数 
+int QueueSize(Queue* pq);
+// 检测队列是否为空，如果为空返回非零结果，如果非空返回0 
+int QueueEmpty(Queue* pq);
+// 销毁队列 
+void QueueDestroy(Queue* pq);
