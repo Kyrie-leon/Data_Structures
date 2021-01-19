792e43e8040b589e1acb13a40e75179f38699388
kyrie-leon <gaomeng9518@163.com>
2021-01-19 23:07:26 +0800

    插入排序

---
+++ Sort/sort.h
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<windows.h>
+#include<time.h>
+#include<stdlib.h>
+
+//打印数组
+void PrintArr(int* a, int n);
+
+// 插入排序
+void InsertSort(int* a, int n);
+
+// 希尔排序
+void ShellSort(int* a, int n);
+
+// 选择排序
+void SelectSort(int* a, int n);
+
+// 堆排序
+void AdjustDwon(int* a, int n, int root);
+void HeapSort(int* a, int n);
+
+// 冒泡排序
+void BubbleSort(int* a, int n);
+
+// 快速排序递归实现
+// 快速排序hoare版本
+int PartSort1(int* a, int left, int right);
+// 快速排序挖坑法
+int PartSort2(int* a, int left, int right);
+// 快速排序前后指针法
+int PartSort3(int* a, int left, int right);
+void QuickSort(int* a, int left, int right);
+
+// 快速排序 非递归实现
+void QuickSortNonR(int* a, int left, int right);
+
+// 归并排序递归实现
+void MergeSort(int* a, int n);
+// 归并排序非递归实现
+void MergeSortNonR(int* a, int n);
+
+// 计数排序
+void CountSort(int* a, int n);
