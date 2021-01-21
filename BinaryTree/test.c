c081c045a01adfbf116ef7ad6f5de19bab958eb3
kyrie-leon <gaomeng9518@163.com>
2021-01-21 18:13:11 +0800

    测试文件

--- BinaryTree/test.c
+++ BinaryTree/test.c
@@ -1,29 +1,39 @@
 #include"BinaryTree.h"
 #include"Queue.h"
 
+void TestTree1()
+{
+	char str[100] = "ABD##E#H##CF##G##";
+	int i = 0;
+	BTNode* node = CreateTree(str, &i);
+	InOrder(node);
+}
+
 int main()
 {
-	BTNode* A = CreateNode('A');
-	BTNode* B = CreateNode('B');
-	BTNode* C = CreateNode('C');
-	BTNode* D = CreateNode('D');
-	BTNode* E = CreateNode('E');
-	//BTNode* F = CreateNode('F');
-	A->_left = B;
-	A->_right = C;
-	//C->_right = F;
-	B->_left = D;
-	B->_right = E;
+	TestTree1();
 
-	//PrevOrder(A);	//Ç°Ğò±éÀú
-	//InOrder(A);	//ÖĞĞò±éÀú
-	//PostOrder(A); //ºóĞò±éÀú
-	//LevelOrder(A);	//²ãĞò±éÀú
-	//printf("%d\n", TreeSize(A));
-	//printf("%d\n", TreeLeafSize(A));
-	//printf("%d\n", TreeComplete(A));
-	printf("%d\n", TreeLevelKSize(A,2));
-	printf("%c\n", TreeFind(A, 'C')->_data);
+	//BTNode* A = CreateNode('A');
+	//BTNode* B = CreateNode('B');
+	//BTNode* C = CreateNode('C');
+	//BTNode* D = CreateNode('D');
+	//BTNode* E = CreateNode('E');
+	////BTNode* F = CreateNode('F');
+	//A->_left = B;
+	//A->_right = C;
+	////C->_right = F;
+	//B->_left = D;
+	//B->_right = E;
+
+	////PrevOrder(A);	//Ç°Ğò±éÀú
+	////InOrder(A);	//ÖĞĞò±éÀú
+	////PostOrder(A); //ºóĞò±éÀú
+	////LevelOrder(A);	//²ãĞò±éÀú
+	////printf("%d\n", TreeSize(A));
+	////printf("%d\n", TreeLeafSize(A));
+	////printf("%d\n", TreeComplete(A));
+	//printf("%d\n", TreeLevelKSize(A,2));
+	//printf("%c\n", TreeFind(A, 'C')->_data);
 
 	system("pause");
 	return 0;
