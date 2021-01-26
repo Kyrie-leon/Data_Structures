d9412b09e96608eda0f38cbb45cc81ec6a3f24fd
kyrie-leon <gaomeng9518@163.com>
2021-01-26 23:19:24 +0800

    排序2

--- Sort/test.c
+++ Sort/test.c
@@ -17,10 +17,30 @@ void TestShellSort()
 	ShellSort(a, n);
 	PrintArr(a, n);
 }
+
+void TestHeapSort()
+{
+	int a[] = { 2,4,7,2,6,4,5,7,8,32,33,11 };
+	int n = sizeof(a) / sizeof(a[0]);
+	PrintArr(a, n);
+	HeapSort(a, n);
+	PrintArr(a, n);
+}
+
+void TestBubbleSort()
+{
+	int a[] = { 2,4,7,2,6,4,5,7,8,32,33,11 };
+	int n = sizeof(a) / sizeof(a[0]);
+	PrintArr(a, n);
+	BubbleSort(a, n);
+	PrintArr(a, n);
+}
 int main()
 {
 	//TestInsertSort();
-	TestShellSort();
+	//TestShellSort();
+	//TestHeapSort();
+	TestBubbleSort();
 	system("pause");
 	return 0;
 }
