e9a4d8f5952694ed50d3a09aac14e73aa2dab6ca
kyrie-leon <gaomeng9518@163.com>
2021-01-28 22:11:30 +0800

    经典排序算法

--- Sort/test.c
+++ Sort/test.c
@@ -29,18 +29,34 @@ void TestHeapSort()
 
 void TestBubbleSort()
 {
+	//int a[] = { 6,25,24,9,12,55,56};
 	int a[] = { 2,4,7,2,6,4,5,7,8,32,33,11 };
 	int n = sizeof(a) / sizeof(a[0]);
+	QuickSort(a, 0, n - 1);
+	printf("-----------");
 	PrintArr(a, n);
-	BubbleSort(a, n);
+	BubbleSort1(a, n);
 	PrintArr(a, n);
 }
+
+void TestQuickSort()
+{
+	int a[] = { 6,1,2,7,9,3,4,5,10,8 };
+	//int a[] = { 2,4,7,2,6,4,5,7,8,32,33,11 };
+	int n = sizeof(a) / sizeof(a[0]);
+
+	PrintArr(a, n);
+	QuickSort(a, 0, n - 1);
+	PrintArr(a, n);
+}
+
 int main()
 {
 	//TestInsertSort();
 	//TestShellSort();
 	//TestHeapSort();
-	TestBubbleSort();
+	//TestBubbleSort();
+	TestQuickSort();
 	system("pause");
 	return 0;
 }
